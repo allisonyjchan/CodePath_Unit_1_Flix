2f3ced08de96c414591b58e3b6b9a64b9539d94d
Allison Chan <allisonchanyijun@gmail.com>
2021-09-27 23:54:25 -0700

    Added collection view

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Allison Chan on 9/27/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+
+    @IBOutlet weak var posterView: UIImageView!
+}
--- Flix/MovieGridViewController.swift
+++ Flix/MovieGridViewController.swift
@@ -6,16 +6,70 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class MovieGridViewController: UIViewController {
+class MovieGridViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var movies = [[String:Any]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
         // Do any additional setup after loading the view.
+        let url = URL(string: "https://api.themoviedb.org/3/movie/566525/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+             // This will run when the network request returns
+             if let error = error {
+                    print(error.localizedDescription)
+             } else if let data = data {
+                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                
+                    // assigning the movies variable to the names of the actual movies
+                    // the as! is how we cast the dataDictionary["results"]
+                    // into a new compatible type
+                    self.movies = dataDictionary["results"] as! [[String:Any]]
+                
+                    // calls the tableViewFunctions again so that movies show up
+                    // without reloading, then they only run once when movie count hasn't been intialized
+                    self.collectionView.reloadData()
+                
+                    // print(dataDictionary)
+                    // TODO: Get the array of movies
+                    // TODO: Store the movies in a property to use elsewhere
+                    // TODO: Reload your table view data
+
+             }
+        }
+        task.resume()
     }
     
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return movies.count
+        }
+        
+        
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieGridCell", for: indexPath) as! MovieGridCell
+            
+            let movie = movies[indexPath.item]
+            
+            let baseUrl = "https://image.tmdb.org/t/p/w185"
+            let posterPath = movie["poster_path"] as! String
+            let posterUrl = URL(string: baseUrl + posterPath)
+            
+            cell.posterView.af_setImage(withURL: posterUrl!)
 
+            
+            return cell
+        }
     /*
     // MARK: - Navigation
 
